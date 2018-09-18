c03b9782abc466051530b5f7993bd8f3e6aaab6c
firumon <github@firumon.com>
2018-09-18 10:40:18 +0530

    Start

---
+++ config/appframe.php
@@ -0,0 +1,64 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Brand Image
+    |--------------------------------------------------------------------------
+    |
+    | An image url to be displayed on top left corner.
+    | NULL or Absolute Url or Relative Url to domain root is only accepted
+    | If image, maximum height should be 32 pixels
+    |
+    */
+
+    'brand' => null,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Brand Text
+    |--------------------------------------------------------------------------
+    |
+    | Text to be displayed on top left corner.
+    | If brand is not NULL(any url given), then this text will be used as Alternate Text to the image
+    |
+    */
+
+    'brand_text' => 'Milestone',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Title
+    |--------------------------------------------------------------------------
+    |
+    | HTML > HEAD > Title
+    |
+    */
+
+    'title' => 'Application framework',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Favicon Url
+    |--------------------------------------------------------------------------
+    |
+    | Url to the favicon to be set as page icon
+    | Tip: Should be transparent (png) and 32x32 size
+    | NULL or Absolute Url or Relative Url to domain root is only accepted
+    |
+    */
+
+    'favicon_url' => 'https://png.icons8.com/office/2x/minecraft-logo.png',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Page Description
+    |--------------------------------------------------------------------------
+    |
+    | HTML HEAD meta description
+    |
+    */
+
+    'page_description' => "",
+];
